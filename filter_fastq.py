d2e9a81056e21d14f6bc7a652933934b66403096
alermol <ermol-2012@yandex.ru>
2021-02-03 12:06:15 +0300

    add -h argument

--- filter_fastq.py
+++ filter_fastq.py
@@ -3,34 +3,6 @@ Authors: Aleksey Ermolaev, Danko Katerina
 
 Script to filter fastq file.
 
-Optional arguments:
-
-    --min_length INT
-    Minimal length for reads to pass the filter. Must be > 0.
-
-    --keep_filtered
-    Flag. Write not passed the filter reads in file.
-
-    --gc_bounds INT INT
-    Takes 1 or 2 values separated by space - lower and upper bounds of
-    GC-content for read to pass the filter. If single value is get its
-    counts as upper bound. Lower bound must lower of equal to upper.
-    Both bounds must be >= 0.
-
-    --output_base_name STR
-    Prefix for output file(s). If not specified the name of original file
-    will be used as a prefix.
-
-
-Positional arguments:
-
-    fastq_file
-    fastq file to process.
-
-
-Example:
-    python fastq_filtrator.py --min_length 50 --keep_filtered
-    --gc_bounds 55 70 --output_base_name output_reads reads.fastq
 '''
 
 import itertools
@@ -39,6 +11,47 @@ from collections import namedtuple
 from pathlib import Path
 
 args = sys.argv[1:]
+
+if len(args) == 1 and '-h' in args:
+    sys.exit(
+        '''
+    Optional arguments:
+
+        --min_length INT
+        Minimal length for reads to pass the filter. Must be > 0.
+
+        --keep_filtered
+        Flag. Write not passed the filter reads in file.
+
+        --gc_bounds INT INT
+        Takes 1 or 2 values separated by space - lower and upper bounds of
+        GC-content for read to pass the filter. If single value is get its
+        counts as upper bound. Lower bound must lower of equal to upper.
+        Both bounds must be >= 0.
+
+        --output_base_name STR
+        Prefix for output file(s). If not specified the name of original file
+        will be used as a prefix.
+
+        -h
+        Show this help
+
+
+    Positional arguments:
+
+        fastq_file
+        fastq file to process.
+
+
+    Example:
+        python3 fastq_filtrator.py --min_length 50 --keep_filtered
+        --gc_bounds 55 70 --output_base_name output_reads reads.fastq
+    '''
+    )
+elif '-h' in args and len(args) > 1:
+    sys.exit('-h can not be used together with other arguments')
+else:
+    sys.exit('Unknown arguments')
 
 try:
     if not Path(args[-1]).exists():
